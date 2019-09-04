2090867a370ba4bd7a7cd3f445c7729aea92faae
Lomash Sharma <lomash.sharma@mountblue.io>
2019-09-04 11:50:51 +0530

    first func created

---
+++ ipl/matchesPlayedPerYear.js
@@ -0,0 +1,15 @@
+function matchesPlayedPerYear(jsonObj) {
+  if (jsonObj.length == 0) {
+    return 0;
+  }
+  var matchesPlayedData = {};
+  jsonObj.filter(match => {
+    if (match.season in matchesPlayedData) {
+        matchesPlayedData[match.season] += 1;
+    } else {
+        matchesPlayedData[match.season] = 1;
+    }
+  });
+    return matchesPlayedData;
+}
+module.exports = matchesPlayedPerYear;
